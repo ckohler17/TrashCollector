c1d7fca83d365166407e6425aaade83171d48325
Carolynn Kohler <carolynn_78@yahoo.com>
2019-11-25 15:18:15 -0600

    initial commit. Added the customer and employee models and created tables

---
+++ TrashCollector/TrashCollector/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string name { get; set; }
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zipCode { get; set; }
+        public string pickUpDay { get; set; }
+        public string startDate { get; set; }
+        public string endDate { get; set; }
+        public bool isSuspended { get; set; }
+
+    }
+}
